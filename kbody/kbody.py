Fix many bugs. An adaptive gradient scaling algorithm is urgently needed.

kbody/kbody.py
@@ -9,7 +9,6 @@ import numpy as np
 import kbody_input
 from itertools import repeat
 from tensorflow.contrib.layers.python.layers import conv2d, flatten
-from tensorflow.contrib.layers.python.layers import initializers
 from tensorflow.python.ops import init_ops
 from kbody_input import SEED
 
@@ -21,12 +20,15 @@ FLAGS = tf.app.flags.FLAGS
 # Basic model parameters.
 tf.app.flags.DEFINE_integer('batch_size', 50,
                             """Number of structures to process in a batch.""")
+tf.app.flags.DEFINE_integer('num_epochs_per_decay', 25,
+                            """Epochs after which learning rate decays.""")
+tf.app.flags.DEFINE_float('learning_rate_decay_factor', 0.95,
+                          """The learning rate decay factor.""")
+tf.app.flags.DEFINE_float('learning_rate', 0.1,
+                          """The initial learning rate.""")
 
 # Constants describing the training process.
 MOVING_AVERAGE_DECAY = 0.9999      # The decay to use for the moving average.
-NUM_EPOCHS_PER_DECAY = 20.0        # Epochs after which learning rate decays.
-LEARNING_RATE_DECAY_FACTOR = 0.98  # Learning rate decay factor.
-INITIAL_LEARNING_RATE = 0.1        # Initial learning rate.
 
 
 def variable_summaries(tensor):
@@ -185,7 +187,7 @@ def _inference_alex_kbody(conv, kbody_term, sizes=(40, 70, 60, 2, 40, 20),
     return energies
 
 
-def _inference_sum_kbody(conv, kbody_term, sizes=(40, 60, 80, 70, 10),
+def _inference_sum_kbody(conv, kbody_term, sizes=(60, 120, 120, 120),
                          verbose=True):
   """
   Infer the k-body term of `sum-kbody-cnn`.
@@ -224,8 +226,8 @@ def _inference_sum_kbody(conv, kbody_term, sizes=(40, 60, 80, 70, 10),
         stride=stride,
         padding=padding,
         scope="Hidden{:d}".format(i + 1),
-        weights_initializer=initializers.xavier_initializer(
-          seed=SEED, dtype=dtype),
+        weights_initializer=init_ops.truncated_normal_initializer(
+          seed=SEED, dtype=dtype, stddev=0.1),
         biases_initializer=init_ops.zeros_initializer(dtype=dtype),
       )
       if verbose:
@@ -235,7 +237,7 @@ def _inference_sum_kbody(conv, kbody_term, sizes=(40, 60, 80, 70, 10),
       conv,
       1,
       kernel_size,
-      activation_fn=None,
+      activation_fn=tf.nn.relu,
       biases_initializer=None,
       weights_initializer=init_ops.truncated_normal_initializer(
         seed=SEED, dtype=dtype, stddev=0.1),
@@ -289,6 +291,8 @@ def inference(input_tensor, offsets, kbody_terms, model='sum-kbody-cnn',
     raise ValueError("The model `{}` is not supported!".format(model))
 
   concat = tf.concat(kbody_energies, axis=1)
+  tf.summary.histogram("kbody_contribs", concat)
+
   if verbose:
     print_activations(concat)
 
@@ -342,9 +346,9 @@ def get_total_loss(energies, pred_energies, atomic=False):
 
   """
   if atomic:
-    scale = tf.constant(FLAGS.num_atoms, tf.float32)
+    scale = tf.constant(FLAGS.num_atoms, tf.float32, name="natoms")
   else:
-    scale = tf.constant(1.0, tf.float32)
+    scale = tf.constant(1.0, tf.float32, name="one")
 
   with tf.name_scope("outputs"):
     with tf.name_scope("batch"):
@@ -365,7 +369,17 @@ def get_total_loss(energies, pred_energies, atomic=False):
   return loss
 
 
-def get_train_op(total_loss, global_step, scale=1.0, clip=False):
+def _get_kbody_scope(var):
+  """
+  Return the kbody scope of the given variable tensor.
+  """
+  if "/" not in var.op.name:
+    return None
+  else:
+    return var.op.name.split("/")[0]
+
+
+def get_train_op(total_loss, global_step, kbody_scales=None):
   """
   Train the Behler model.
 
@@ -376,8 +390,7 @@ def get_train_op(total_loss, global_step, scale=1.0, clip=False):
     total_loss: the total loss Tensor.
     global_step: Integer Variable counting the number of training steps
       processed.
-    num_kernels: a Tensor of `int` as the total number of 1x1 kernels.
-    clip: boolena indicating explicitly clip gradients or not.
+    kbody_scales: a dict, the scale Tensors for the kbody terms.
 
   Returns:
     train_op: op for training.
@@ -387,34 +400,35 @@ def get_train_op(total_loss, global_step, scale=1.0, clip=False):
   # Variables that affect learning rate.
   train_size = FLAGS.num_examples * 0.8
   num_batches_per_epoch = train_size // FLAGS.batch_size
-  decay_steps = int(num_batches_per_epoch * NUM_EPOCHS_PER_DECAY)
+  decay_steps = int(num_batches_per_epoch * FLAGS.num_epochs_per_decay)
 
   # Decay the learning rate exponentially based on the number of steps.
-  lr = tf.train.exponential_decay(INITIAL_LEARNING_RATE,
+  lr = tf.train.exponential_decay(FLAGS.learning_rate,
                                   global_step,
                                   decay_steps,
-                                  LEARNING_RATE_DECAY_FACTOR,
+                                  FLAGS.learning_rate_decay_factor,
                                   staircase=True)
   tf.summary.scalar('learning_rate', lr)
 
   # Compute gradients.
-  opt = tf.train.AdamOptimizer(lr)
-  grads = opt.compute_gradients(total_loss)
-
-  # Compute the 2-norm of the gradients
-  # norms = {}
-  # for grad, var in grads:
-  #   norms[var] = tf.norm(grad, name=var.op.name + '/gnorm')
-  # norm = tf.add_n(list(norms.values()), name="gnorm")
-  # scale = tf.minimum(tf.maximum(scale, 1.0 / norm), 1.0)
-  # tf.summary.scalar('grad_scale', scale)
+  opt = tf.train.MomentumOptimizer(lr, 0.8)
 
   # Explicitly scale the gradients.
-  # grads = [(grad * scale, var) for grad, var in grads]
+  if kbody_scales is None:
+    kbody_scales = {}
+  grads = []
+  for grad, var in opt.compute_gradients(total_loss):
+    scope = _get_kbody_scope(var)
+    scale = kbody_scales.get(scope)
+    if scale is None:
+      grads.append((grad, var))
+    else:
+      grads.append((grad * scale, var))
 
-  # Explicitly clip the gradients to avoid explodes!
-  if clip:
-    grads = [(tf.clip_by_value(grad, -1., 1.), var) for grad, var in grads]
+  # Add histograms for grandients
+  for grad, var in grads:
+    tf.summary.histogram(var.op.name + "/gradients", grad)
+    tf.summary.scalar(var.op.name + "/grad_norm", tf.norm(grad))
 
   # Apply gradients.
   apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
@@ -422,12 +436,6 @@ def get_train_op(total_loss, global_step, scale=1.0, clip=False):
   # Add histograms for trainable variables.
   for var in tf.trainable_variables():
     tf.summary.histogram(var.op.name, var)
-
-  # Add histograms and norms for gradients.
-  for grad, var in grads:
-    if grad is not None:
-      tf.summary.histogram(var.op.name + '/gradients', grad)
-      # tf.summary.scalar(var.op.name + '/gnorm', norms[var])
 
   # Track the moving averages of all trainable variables.
   variable_averages = tf.train.ExponentialMovingAverage(

kbody/kbody_train.py
@@ -20,7 +20,7 @@ FLAGS = tf.app.flags.FLAGS
 # Basic model parameters.
 tf.app.flags.DEFINE_string('train_dir', './events',
                            """The directory for storing training files.""")
-tf.app.flags.DEFINE_integer('max_steps', 10000,
+tf.app.flags.DEFINE_integer('max_steps', 50000,
                             """The maximum number of training steps.""")
 tf.app.flags.DEFINE_integer('save_frequency', 20,
                             """The frequency, in number of global steps, that
@@ -30,13 +30,14 @@ tf.app.flags.DEFINE_integer('log_frequency', 100,
                             the training progress wiil be logged.""")
 tf.app.flags.DEFINE_boolean('log_device_placement', False,
                             """Whether to log device placement.""")
-tf.app.flags.DEFINE_boolean('restart', True,
-                            """Restore the latest checkpoint if possible.""")
+tf.app.flags.DEFINE_boolean('restart', False,
+                            """Restart the training and reset the global step
+                            and learning rate.""")
 tf.app.flags.DEFINE_boolean('timeline', False,
                             """Enable timeline profiling if True.""")
 
 
-def train_model(*args):
+def train_model(unused):
 
   with tf.Graph().as_default():
     global_step = tf.contrib.framework.get_or_create_global_step()
@@ -44,17 +45,15 @@ def train_model(*args):
     # Read dataset configurations
     settings = kbody.inputs_settings(train=True)
     offsets = settings["kbody_term_sizes"]
-
-    # Remove all commas because tensorflow scope name can not contain such
-    # characters.
-    kbody_terms = [x.replace(",", "") for x in settings["kbody_terms"]]
+    kbody_terms = []
+    scales = {}
+    for i, kbody_term in enumerate(settings["kbody_terms"]):
+      scope = kbody_term.replace(",", "")
+      kbody_terms.append(scope)
+      scales[scope] = tf.constant(1.0, name="inv%s" % scope)
 
     # Get features and energies.
     features, energies = kbody.inputs(train=True)
-
-    # Set the scaling factor
-    num_kernels = features.get_shape().as_list()[2] * len(offsets)
-    scale = tf.constant(1.0 / num_kernels, dtype=tf.float32)
 
     # Build a Graph that computes the logits predictions from the
     # inference model.
@@ -71,7 +70,7 @@ def train_model(*args):
 
     # Build a Graph that trains the model with one batch of examples and
     # updates the model parameters.
-    train_op = kbody.get_train_op(loss, global_step, scale=scale, clip=False)
+    train_op = kbody.get_train_op(loss, global_step, kbody_scales=scales)
 
     class _LoggerHook(tf.train.SessionRunHook):
       """ Logs loss and runtime."""
@@ -80,6 +79,7 @@ def train_model(*args):
         super(_LoggerHook, self).__init__()
         self._step = -1
         self._start_time = 0
+        self._epoch = 0.0
         self._log_frequency = FLAGS.log_frequency
 
       def begin(self):
@@ -87,6 +87,7 @@ def train_model(*args):
 
       def before_run(self, run_context):
         self._step += 1
+        self._epoch = self._step / (FLAGS.num_examples * 0.8 / FLAGS.batch_size)
         self._start_time = time.time()
         return tf.train.SessionRunArgs({"loss": loss})
 
@@ -100,12 +101,11 @@ def train_model(*args):
         if self.should_log():
           examples_per_sec = num_examples_per_step / duration
           sec_per_batch = float(duration)
-          fstr = ('%s: step %6d, loss = %10.6f (%6.1f examples/sec; %7.3f '
-                  'sec/batch)')
-          print(fstr % (datetime.now(), self._step, loss_value,
-                        examples_per_sec, sec_per_batch))
+          format_str = "%s: step %6d, epoch=%7.2f, loss = %10.6f " \
+                       "(%6.1f examples/sec; %7.3f sec/batch)"
+          print(format_str % (datetime.now(), self._step, self._epoch,
+                              loss_value, examples_per_sec, sec_per_batch))
 
-    saver = tf.train.Saver()
     run_meta = tf.RunMetadata()
     run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
 
@@ -133,6 +133,13 @@ def train_model(*args):
           with open(self.get_ctf(), "w+") as f:
             f.write(ctf)
 
+    if FLAGS.restart:
+      reset_op = tf.assign(global_step, 0)
+      reset = True
+    else:
+      reset_op = tf.no_op("skip")
+      reset = False
+
     with tf.train.MonitoredTrainingSession(
         checkpoint_dir=FLAGS.train_dir,
         save_summaries_steps=FLAGS.save_frequency,
@@ -140,15 +147,14 @@ def train_model(*args):
                tf.train.NanTensorHook(loss),
                _LoggerHook(),
                _TimelineHook()],
+        scaffold=None,
         config=tf.ConfigProto(
           log_device_placement=FLAGS.log_device_placement)) as mon_sess:
 
-      # Manually restore the previous checkpoint.
-      if FLAGS.restart:
-        ckpt = tf.train.get_checkpoint_state(FLAGS.train_dir)
-        if ckpt and ckpt.model_checkpoint_path:
-          saver.restore(mon_sess, ckpt.model_checkpoint_path)
       while not mon_sess.should_stop():
+        if reset:
+          mon_sess.run(reset_op)
+          reset = False
         if FLAGS.timeline:
           mon_sess.run(train_op, options=run_options, run_metadata=run_meta)
         else:

kbody/kbody_transform.py
@@ -107,6 +107,33 @@ def map_indices(species, kbody_terms):
   return mapping, selections
 
 
+def get_sort_indices(orders):
+  """
+  Generate the soring indices.
+
+  Args:
+    orders: a List as the ordered many-body atomic symbol combiantions.
+
+  Returns:
+    indices: a dict of indices for sorting along the last axis of the input
+      features.
+
+  """
+  indices = {}
+  for order in orders:
+    elements = list(sorted(order.split(",")))
+    atom_pairs = list(combinations(elements, r=2))
+    n = len(atom_pairs)
+    counter = Counter(atom_pairs)
+    if max(counter.values()) == 1:
+      continue
+    indices[order] = []
+    for pair, times in counter.items():
+      if times > 1:
+        indices[order].append([i for i in range(n) if atom_pairs[i] == pair])
+  return indices
+
+
 def _exponential(d, s):
   """
   Do the exponential scaling on the given array `d`.
@@ -127,7 +154,8 @@ def _bytes_feature(value):
   return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
 
-def transform_and_save(coordinates, energies, species, orders, filename):
+def transform_and_save(coordinates, energies, species, orders, filename,
+                       sort=False):
   """
   Transform the given atomic coordinates to input features and save them to
   tfrecords files using `tf.TFRecordWriter`.
@@ -138,6 +166,7 @@ def transform_and_save(coordinates, energies, species, orders, filename):
     species: a List as the ordered atomic symboles.
     orders: a List as the ordered many-body atomic symbol combiantions.
     filename: a str, the file to save examples.
+    sort: a boolean indicating whether sorting the input features or not.
 
   """
 
@@ -154,6 +183,11 @@ def transform_and_save(coordinates, energies, species, orders, filename):
   offsets.append(-1)
   offsets = np.asarray(offsets, dtype=np.int64)
   sizes = np.diff(offsets[:-1])
+
+  if sort:
+    sort_indices = get_sort_indices(orders)
+  else:
+    sort_indices = {}
 
   sample = np.zeros((cnk, ck2), dtype=coordinates.dtype)
   num_traj = len(coordinates)
@@ -174,6 +208,17 @@ def transform_and_save(coordinates, energies, species, orders, filename):
     for j, order in enumerate(orders):
       for k in range(ck2):
         sample[offsets[j]: offsets[j + 1], k] = rr[mapping[order][k]]
+
+    sample -= np.exp(-3.0)
+    sample[sample < 0.0] = 0.0
+
+    if sort:
+      for j, order in enumerate(orders):
+        for ix in sort_indices.get(order, []):
+          z = sample[offsets[j]: offsets[j + 1], ix]
+          z.sort(axis=1)
+          sample[offsets[j]: offsets[j + 1], ix] = z
+
     x = sample.tostring()
     y = np.atleast_2d(energies[i]).tostring()
     example = tf.train.Example(
@@ -225,6 +270,6 @@ def _test_split_tensor():
     assert np.linalg.norm(values[0] - raw_inputs[:, :, 4:6, :]) == 0.0
 
 
-if __name__ == "__main__":
-  _test_map_indices()
-  _test_split_tensor()
+# if __name__ == "__main__":
+#   _test_map_indices()
+#   _test_split_tensor()