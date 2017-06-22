721f231585a61d1f4a6130bda7d1bce0cabbff4c
Bismarrck <Bismarrck@me.com>
2017-06-22 13:52:56 +0200

    The input module is compatible with the latest changes of `extract_xyz`. The code quality is also improved.

--- kbody/kbody_input.py
+++ kbody/kbody_input.py
@@ -5,19 +5,15 @@ This script is used to building training and validation datasets.
 
 from __future__ import print_function, absolute_import
 
-import tensorflow as tf
-import numpy as np
-import re
-import time
-import sys
 import json
-import kbody_transform
-from os.path import join, isfile, isdir
-from os import makedirs
-from sklearn.model_selection import train_test_split
-from scipy.misc import comb
-from collections import Counter
 from functools import partial
+from os import makedirs
+from os.path import join, isfile, isdir
+import numpy as np
+import tensorflow as tf
+from scipy.misc import comb
+import kbody_transform
+from xyz import extract_xyz
 
 __author__ = 'Xin Chen'
 __email__ = 'Bismarrck@me.com'
@@ -55,174 +51,14 @@ tf.app.flags.DEFINE_float('weighted_loss', None,
 
 FLAGS = tf.app.flags.FLAGS
 
-# Set the random state
-SEED = 218
 
-# Hartree to kcal/mol
-hartree_to_kcal = 627.509
-
-# Hartree to eV
-hartree_to_ev = 27.211
-
-# a.u to angstroms
-au_to_angstrom = 0.52917721092
-
-
-def exp_loss_weight_fn(x, x0=0.0, beta=1.0):
+def exp_rmse_loss_fn(x, x0=0.0, beta=1.0):
   """
   An exponential function for computing the weighted loss.
 
   I.e. \\(y = \e^{-\beta \cdot (x - x_0)}\\).
   """
   return np.float32(np.exp(-(x - x0) * beta))
-
-
-def _get_regex_patt_and_unit(xyz_format):
-  """
-  Return the corresponding regex patterns and the energy unit.
-  
-  Args:
-    xyz_format: a `str` as the format of the file. 
-
-  Returns:
-    energy_patt: a regex pattern for parsing energies.
-    string_patt: a regex pattern for parsing atomic symbols and coordinates.
-    unit: a `float` transforming the energies to eV.
-    parse_forces: a `bool` indicating whether the file supports parsing forces.
-
-  """
-  if xyz_format.lower() == 'grendel':
-    energy_patt = re.compile(r"Lattice=\"(.*)\".*"
-                             r"energy=([\d.-]+)\s+pbc=\"(.*)\"")
-    string_patt = re.compile(
-      r"([A-Za-z]{1,2})\s+([\d.-]+)\s+([\d.-]+)\s+([\d.-]+)\s+"
-      "\d+\s+\d.\d+\s+\d+\s+([\d.-]+)\s+([\d.-]+)\s+([\d.-]+)")
-    unit = 1.0
-    parse_forces = True
-  elif xyz_format.lower() == 'cp2k':
-    energy_patt = re.compile(r"i\s=\s+\d+,\sE\s=\s+([\w.-]+)")
-    string_patt = re.compile(
-      r"([A-Za-z]+)\s+([\w.-]+)\s+([\w.-]+)\s+([\w.-]+)")
-    parse_forces = False
-    unit = hartree_to_ev
-  elif xyz_format.lower() == 'xyz':
-    energy_patt = re.compile(r"([\w.-]+)")
-    string_patt = re.compile(
-      r"([A-Za-z]+)\s+([\w.-]+)\s+([\w.-]+)\s+([\w.-]+)")
-    unit = hartree_to_ev
-    parse_forces = False
-  elif xyz_format.lower() == 'extxyz':
-    energy_patt = re.compile(r"i=(\d+).(\d+),\sE=([\d.-]+)")
-    string_patt = re.compile(
-      r"([A-Za-z]+)\s+([\w.-]+)\s+([\w.-]+)\s+([\w.-]+)")
-    unit = hartree_to_ev
-    parse_forces = False
-  else:
-    raise ValueError("The file format of %s is not supported!" % xyz_format)
-  if FLAGS.unit is not None:
-    unit = FLAGS.unit
-  return energy_patt, string_patt, unit, parse_forces
-
-
-def extract_xyz(filename, num_examples, num_atoms, xyz_format='xyz',
-                verbose=True):
-  """
-  Extract atomic species, energies, coordiantes, and perhaps forces, from the 
-  file.
-
-  Args:
-    filename: a `str` as the file to parse.
-    num_examples: a `int` as the maximum number of examples to parse.
-    num_atoms: a `int` as the number of atoms. If `mixed` is True, this should 
-      be the maximum number of atoms in one configuration.
-    xyz_format: a `str` representing the format of the given xyz file.
-    verbose: a `bool` indicating whether we should log the parsing progress.
-
-  Returns
-    array_of_species: an array of `Array[str]` as the species of molecules.
-    energies: `Array[N, ]`, a 1D array as the atomic energies.
-    coordinates: `Array[N, ...]`, a 3D array as the atomic coordinates.
-    forces: `Array[N, ...]`, a 3D array as the atomic forces.
-
-  """
-
-  dtype = np.float32
-  energies = np.zeros((num_examples,), dtype=np.float64)
-  coords = np.zeros((num_examples, num_atoms, 3), dtype=dtype)
-  lattices = np.zeros((num_examples, 9))
-  pbcs = np.zeros((num_examples, 3), dtype=bool)
-  array_of_species = []
-  species = []
-  stage = 0
-  i = 0
-  j = 0
-  n = None
-  ener_patt, xyz_patt, unit, parse_forces = _get_regex_patt_and_unit(xyz_format)
-
-  if parse_forces:
-    forces = np.zeros((num_examples, num_atoms, 3), dtype=dtype)
-  else:
-    forces = None
-
-  tic = time.time()
-  if verbose:
-    sys.stdout.write("Extract cartesian coordinates ...\n")
-  with open(filename) as f:
-    for line in f:
-      if i == num_examples:
-        break
-      l = line.strip()
-      if l == "":
-        continue
-      if stage == 0:
-        if l.isdigit():
-          n = int(l)
-          if n > num_atoms:
-            raise ValueError("The number of atoms %d from the file is larger "
-                             "than the given maximum %d!" % (n, num_atoms))
-          stage += 1
-      elif stage == 1:
-        m = ener_patt.search(l)
-        if m:
-          if xyz_format.lower() == 'extxyz':
-            energies[i] = float(m.group(3)) * unit
-          elif xyz_format.lower() == 'grendel':
-            energies[i] = float(m.group(2)) * unit
-            lattices[i] = [float(x) for x in m.group(1).split()]
-            pbcs[i] = [True if x == "T" else False for x in m.group(3).split()]
-          else:
-            energies[i] = float(m.group(1)) * unit
-          stage += 1
-      elif stage == 2:
-        m = xyz_patt.search(l)
-        if m:
-          coords[i, j, :] = [float(v) for v in m.groups()[1:4]]
-          if parse_forces:
-            forces[i, j, :] = [float(v) for v in m.groups()[4:7]]
-          species.append(m.group(1))
-          j += 1
-          if j == n:
-            array_of_species.append(species)
-            species = []
-            j = 0
-            stage = 0
-            i += 1
-            if verbose and i % 1000 == 0:
-              sys.stdout.write("\rProgress: %7d  /  %7d" % (i, num_examples))
-    if verbose:
-      print("")
-      print("Total time: %.3f s\n" % (time.time() - tic))
-
-  if i < num_examples:
-    array_of_species = np.asarray(array_of_species)
-    energies = np.resize(energies, (i, ))
-    coords = np.resize(coords, (i, num_atoms, 3))
-    if forces is not None:
-      forces = np.resize(forces, (i, num_atoms, 3))
-    lattices = np.resize(lattices, (i, 9))
-    pbcs = np.resize(pbcs, (i, 3))
-
-  return array_of_species, energies, coords, forces, lattices, pbcs
 
 
 def get_filenames(train=True, dataset=None):
@@ -274,72 +110,39 @@ def may_build_dataset(dataset=None, verbose=True):
   if not isfile(xyzfile):
     raise IOError("The dataset file %s can not be accessed!" % xyzfile)
 
-  # Read atomic symbols, DFT energies, atomic coordinates and atomic forces from
-  # the xyz file.
-  species, energies, coordinates, forces, lattices, pbcs = extract_xyz(
-    xyzfile,
-    num_examples=FLAGS.num_examples,
-    num_atoms=FLAGS.num_atoms,
-    verbose=verbose,
-    xyz_format=FLAGS.format,
-  )
-  min_ener = energies.min()
-  indices = list(range(len(coordinates)))
+  # Extract the xyz file and split it into two sets: a training set and a
+  # testing set.
+  xyz = extract_xyz(xyzfile, num_examples=FLAGS.num_examples, verbose=verbose,
+                    num_atoms=FLAGS.num_atoms, xyz_format=FLAGS.format)
+  xyz.split(test_size=FLAGS.test_size)
 
-  # Transform the coordinates to input features and save these features in a
-  # tfrecords file.
+  # The maximum supported `k` is 5.
   many_body_k = min(5, FLAGS.many_body_k)
 
-  # Split the energies and coordinates into two sets: a training set and a
-  # testing set. The indices are used for post-analysis.
-  (coords_train, coords_test,
-   energies_train, energies_test,
-   indices_train, indices_test,
-   species_train, species_test,
-   lattices_train, lattices_test,
-   pbcs_train, pbcs_test) = train_test_split(
-    coordinates,
-    energies,
-    indices,
-    species,
-    lattices,
-    pbcs,
-    test_size=FLAGS.test_size,
-    random_state=SEED
-  )
+  # Determine the maximum occurances of each atom type.
+  max_occurs = xyz.get_max_occurs()
 
-  # Determine the maximum occurance of each atom type.
-  max_occurs = {}
-  for symbols in species:
-    c = Counter(symbols)
-    for specie, times in c.items():
-      max_occurs[specie] = max(max_occurs.get(specie, 0), times)
-
-  # Setup the function for computing weighted losses.
+  # Setup the exponential-scaled RMSE.
   if FLAGS.weighted_loss is None:
-    loss_weight_fn = None
+    exp_rmse_fn = None
   else:
+    min_ener, _ = xyz.energy_range
     beta = 1.0 / FLAGS.weighted_loss
-    loss_weight_fn = partial(exp_loss_weight_fn, x0=min_ener, beta=beta)
+    exp_rmse_fn = partial(exp_rmse_loss_fn, x0=min_ener, beta=beta)
 
   # Use a `FixedLenMultiTransformer` to generate features because it will be
   # much easier if the all input samples are fixed-length.
   clf = kbody_transform.FixedLenMultiTransformer(
-    max_occurs,
-    many_body_k=many_body_k,
-    periodic=FLAGS.periodic,
-    order=FLAGS.order,
-    two_body=FLAGS.two_body,
+    max_occurs, many_body_k=many_body_k, periodic=FLAGS.periodic,
+    order=FLAGS.order, two_body=FLAGS.two_body,
   )
-  clf.transform_and_save(species_test, energies_test, coords_test,
-                         test_file, indices_test, lattices_test, pbcs_test,
-                         one_body_weights=False, loss_weight_fn=loss_weight_fn)
-  clf.transform_and_save(species_train, energies_train, coords_train,
-                         train_file, indices_train, lattices_train, pbcs_train,
-                         one_body_weights=True, loss_weight_fn=loss_weight_fn)
+  clf.transform_and_save(
+    xyz, train_file=train_file, test_file=test_file, verbose=True,
+    exp_rmse_fn=exp_rmse_fn
+  )
 
 
-def read_and_decode(filename_queue, cnk, ck2, nat):
+def read_and_decode(filename_queue, cnk, ck2, num_atom_types):
   """
   Read and decode the mixed binary dataset file.
 
@@ -347,12 +150,15 @@ def read_and_decode(filename_queue, cnk, ck2, nat):
     filename_queue: an input queue.
     cnk: a `int` as the value of C(N,k). 
     ck2: a `int` as the value of C(k,2). 
-    nat: a `int` as the number of atom types.
+    num_atom_types: a `int` as the number of atom types.
 
   Returns:
-    features: a 3D array of shape [1, cnk, ck2] as one input feature matrix.
-    energy: a 1D array of shape [1,] as the target for the features.
-    weights: a 1D array of shape [cnk,] as the weights of the k-body contribs.
+    features: a `float32` tensor of shape `(1, cnk, ck2)` as an input matrix.
+    energy: a `float64` tensor as the true energy.
+    occurs: a `float32` tensor of shape `(1, 1, num_atom_types)`.
+    weights: a `float32` tensor of shape `[1, cnk, 1]` as the weights of the
+      k-body contribs.
+    y_weight: a `float32` tensor as the weight of the loss of this sample.
 
   """
   reader = tf.TFRecordReader()
@@ -378,16 +184,16 @@ def read_and_decode(filename_queue, cnk, ck2, nat):
   energy = tf.squeeze(energy)
 
   occurs = tf.decode_raw(example['occurs'], tf.float32)
-  occurs.set_shape([nat])
-  occurs = tf.reshape(occurs, [1, 1, nat])
+  occurs.set_shape([num_atom_types])
+  occurs = tf.reshape(occurs, [1, 1, num_atom_types])
 
   weights = tf.decode_raw(example['weights'], tf.float32)
   weights.set_shape([cnk, ])
   weights = tf.reshape(weights, [1, cnk, 1])
 
-  loss_weight = tf.cast(example['loss_weight'], tf.float32)
+  y_weight = tf.cast(example['loss_weight'], tf.float32)
 
-  return features, energy, occurs, weights, loss_weight
+  return features, energy, occurs, weights, y_weight
 
 
 def inputs(train, batch_size=50, shuffle=True, dataset=None):
--- kbody/kbody_input_tests.py
+++ kbody/kbody_input_tests.py
@@ -12,11 +12,12 @@ from scipy.io import loadmat
 from scipy.misc import comb
 from os.path import join, isfile, dirname
 from os import remove
-from collections import Counter
 from functools import partial
 from sklearn.metrics import pairwise_distances
-from kbody_input import extract_xyz, inputs, exp_loss_weight_fn
-from kbody_input import FLAGS, hartree_to_ev, au_to_angstrom
+from kbody_input import inputs, exp_rmse_loss_fn
+from xyz import extract_xyz
+from kbody_input import FLAGS
+from constants import hartree_to_ev, au_to_angstrom
 from kbody_transform import FixedLenMultiTransformer
 
 
@@ -25,22 +26,19 @@ def test_extract_mixed_xyz():
   Test parsing the mixed xyz file `qm7.xyz`.
   """
   mixed_file = join(dirname(__file__), "..", "datasets", "qm7.xyz")
-  array_of_species, energies, coords, _, _, _ = extract_xyz(
-    mixed_file,
-    num_examples=100000,
-    num_atoms=23,
-  )
+  xyz = extract_xyz(mixed_file, num_examples=100000, num_atoms=23)
   qm7_mat = join(dirname(__file__), "..", "datasets", "qm7.mat")
   ar = loadmat(qm7_mat)
   kcal_to_hartree = 1.0 / 627.509474
   t = ar["T"].flatten() * kcal_to_hartree * hartree_to_ev
   r = np.multiply(ar["R"], au_to_angstrom)
-  lefts = np.ones(len(array_of_species), dtype=bool)
-  for i in range(len(array_of_species)):
-    j = np.argmin(np.abs(energies[i] - t))
+  lefts = np.ones(xyz.num_examples, dtype=bool)
+  for i in range(xyz.num_examples):
+    species, energy, coords = xyz[i][:3]
+    j = np.argmin(np.abs(energy - t))
     if lefts[j]:
-      n = len(array_of_species[i])
-      d = np.linalg.norm(coords[i][:n] - r[j][:n])
+      n = len(species)
+      d = np.linalg.norm(coords[:n] - r[j][:n])
       if d < 0.1:
         lefts[j] = False
   assert not np.all(lefts)
@@ -50,36 +48,20 @@ def test_build_dataset():
   """
   Test building the mixed QM7 dataset.
   """
+  l = 1.5
   dataset = 'qm7.test'
   xyzfile = join("..", "datasets", "qm7.xyz")
-  array_of_species, energies, coordinates, _, _, _ = extract_xyz(
-    xyzfile,
-    num_examples=7165,
-    num_atoms=23,
-    verbose=False,
-  )
-  indices = list(range(len(coordinates)))
-
-  r_hh = 0.64
+  xyz = extract_xyz(xyzfile, num_examples=7165, num_atoms=23, verbose=False)
   many_body_k = 3
-  max_occurs = {}
-  for symbols in array_of_species:
-    c = Counter(symbols)
-    for specie, times in c.items():
-      max_occurs[specie] = max(max_occurs.get(specie, 0), times)
-
   clf = FixedLenMultiTransformer(
-    max_occurs, many_body_k=many_body_k, periodic=False
+    xyz.get_max_occurs(), many_body_k=many_body_k, periodic=False
   )
-
   train_file = join(FLAGS.binary_dir, "{}-train.tfrecords".format(dataset))
   clf.transform_and_save(
-    array_of_species,
-    energies,
-    coordinates,
-    train_file,
-    indices=indices,
+    xyz, train_file=train_file, verbose=True
   )
+
+  species, y_true, coords, = xyz[xyz.indices_of_training[1]][:3]
 
   with tf.Session() as sess:
 
@@ -90,15 +72,14 @@ def test_build_dataset():
     # Features
     # --------
     features, weights = sess.run([batch[0], batch[3]])
-    species = array_of_species[1]
-    n = len(species)
-    coords = coordinates[1][:n]
-    dists = pairwise_distances(coords[2:5])
-    v = np.sort(np.exp(-dists / r_hh)[[0, 0, 1], [1, 2, 2]])
+    natoms = len(species)
+    coords = coords[:natoms]
+    dists = pairwise_distances(coords[:3])
+    v = np.sort(np.exp(-dists / l)[[0, 0, 1], [1, 2, 2]])
 
-    # 2289 is the starting index of `HHH`.
-    assert np.linalg.norm(v - features[1, 0, 2289]) < 0.001
-    assert np.abs(weights[1].flatten().sum() - comb(n, 3)) < 0.001
+    # 0 is the starting index of `CCC`.
+    assert np.linalg.norm(v - features[1, 0, 0]) < 0.001
+    assert np.abs(weights[1].flatten().sum() - comb(natoms, 3)) < 0.001
 
     time.sleep(5)
 
@@ -117,48 +98,33 @@ def test_compute_loss_weight():
   if not isfile(xyzfile):
     raise IOError("The dataset file %s can not be accessed!" % xyzfile)
 
-  array_of_species, energies, coordinates, _, _, _ = extract_xyz(
-    xyzfile,
-    xyz_format='grendel',
-    num_examples=5000,
-    num_atoms=17,
-    verbose=False,
-  )
-  indices = list(range(len(coordinates)))
-  min_ener = energies.min()
+  xyz = extract_xyz(xyzfile, xyz_format='grendel', num_examples=5000,
+                    num_atoms=17, verbose=False)
+  min_ener, _ = xyz.energy_range
 
   many_body_k = 3
-  max_occurs = {}
-  for symbols in array_of_species:
-    c = Counter(symbols)
-    for specie, times in c.items():
-      max_occurs[specie] = max(max_occurs.get(specie, 0), times)
 
   clf = FixedLenMultiTransformer(
-    max_occurs, many_body_k=many_body_k, periodic=False
+    xyz.get_max_occurs(), many_body_k=many_body_k, periodic=False
   )
 
   beta = 1.0 / 10.0
-  loss_weight_fn = partial(exp_loss_weight_fn, x0=min_ener, beta=beta)
+  exp_loss_fn = partial(exp_rmse_loss_fn, x0=min_ener, beta=beta)
 
   train_file = join(FLAGS.binary_dir, "{}-train.tfrecords".format(dataset))
   clf.transform_and_save(
-    array_of_species,
-    energies,
-    coordinates,
-    train_file,
-    indices=indices,
-    loss_weight_fn=loss_weight_fn
+    xyz,
+    train_file=train_file,
+    exp_rmse_fn=exp_loss_fn
   )
+  y_true = xyz.get_training_samples()[1]
 
   with tf.Session() as sess:
-
     batch = inputs(train=True, shuffle=False, batch_size=5, dataset=dataset)
     tf.train.start_queue_runners(sess=sess)
-
-    loss_weights = sess.run(batch[4])
+    y_weights = sess.run(batch[4])
     for i in range(5):
-      assert np.abs(loss_weight_fn(energies[i]) - loss_weights[i]) < 0.000001
+      assert np.abs(exp_loss_fn(y_true[i]) - y_weights[i]) < 0.000001
 
     time.sleep(5)
 
