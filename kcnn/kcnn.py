Fix some bugs of batch normalization.

kcnn/kcnn.py
@@ -406,8 +406,10 @@ def get_train_op(total_loss, global_step):
   # Add the update ops if batch_norm is True.
   # If we don't include the update ops as dependencies on the train step, the
   # batch_normalization layers won't update their population statistics, which
-  # will cause the model to fail at inference time
+  # will cause the model to fail at inference time.
   dependencies = [loss_averages_op]
+  if FLAGS.batch_norm:
+    dependencies.extend(tf.get_collection(tf.GraphKeys.UPDATE_OPS))
 
   # Compute gradients.
   with tf.control_dependencies(dependencies):