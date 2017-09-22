3881df3831f4d160b818e146944097afb87a1004
Bismarrck <Bismarrck@me.com>
2017-09-22 09:27:51 +0200

    Minor update.

--- kcnn/utils.py
+++ kcnn/utils.py
@@ -124,10 +124,12 @@ def selu(x, name=None):
     https://arxiv.org/pdf/1706.02515.pdf
 
   """
-  with ops.name_scope(name, "selu", [x]):
+  with ops.name_scope(name, "SeLU", [x]):
     alpha = 1.6732632423543772848170429916717
     scale = 1.0507009873554804934193349852946
-    return scale * tf.where(x >= 0.0, x, alpha * tf.nn.elu(x))
+    elu = tf.nn.elu(x, name='elu')
+    z = tf.multiply(elu, alpha, name='z')
+    return scale * tf.where(x >= 0.0, x, z, name='selu')
 
 
 def selu_initializer(dtype=tf.float32, seed=None):
